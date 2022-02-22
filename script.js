9d3a73da13fa241c92491fc80d285f11e75008cb
Noah Reece <noah.w.reece@gmail.com>
2022-02-22 00:59:45 -0500

    added js to update infographic when thumbnails clicked. Commented out Div borders and tidied up formatting with some div relocations and setting a max width for the body

--- script.js
+++ script.js
@@ -2,3 +2,28 @@
 // h2.textContent = "This content added by JavaScript";
 
 // document.querySelector("body").appendChild(h2);
+
+const viewport = document.getElementById("infographic");
+
+const thumb_cavern = document.getElementById("thumbnail-cavern");
+const thumb_tower = document.getElementById("thumbnail-tower");
+const thumb_ghost = document.getElementById("thumbnail-ghost");
+const thumb_beware = document.getElementById("thumbnail-beware");
+
+
+
+thumb_cavern.onclick = function() {
+    viewport.src = "./images/info/CavernInfo.jpg";
+}
+
+thumb_tower.onclick = function() {
+    viewport.src = "./images/info/OutpostTowerInfo.jpg";
+}
+
+thumb_ghost.onclick = function() {
+    viewport.src = "./images/info/GetGhostingInfo.jpg";
+}
+
+thumb_beware.onclick = function() {
+    viewport.src = "./images/info/BewareLittleChildrenInfo.jpg";
+}
